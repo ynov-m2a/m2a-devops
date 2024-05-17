77705fe4e5eef88a66a51f0dbf11f31def496508
Lucas SAISSI <lucas.saissi@mgss2i.com>
2024-05-17 09:37:11 +0200

    feat: tfstate in storage account

--- providers.tf
+++ providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~>3.0"
     }
   }
+  backend "azurerm" {
+      resource_group_name  = "m2a-tfstate-rg"
+      storage_account_name = "m2atfstate28351"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
