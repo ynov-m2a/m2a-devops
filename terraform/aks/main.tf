5b78a98e800d8d6d2352c745f9411caf1ab99320
Lucas SAISSI <lucas.saissi@gmail.com>
2024-06-03 11:26:05 +0200

    feat: scale for testing purpose

--- terraform/aks/main.tf
+++ terraform/aks/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_kubernetes_cluster" "aks-cluster" {
 
   default_node_pool {
     name           = "akspool"
-    node_count     = 1
+    node_count     = 2
     vm_size        = "Standard_B2s"
     vnet_subnet_id = azurerm_subnet.aks-subnet.id
   }
